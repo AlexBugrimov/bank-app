7f813d2ea2588ff8f07e71820531ddee154d0fcb
Alex <bugrimov_a@bk.ru>
2020-02-01 22:50:55 +0300

    [#11] Реализован метод getAccountsByClientName

--- src/main/java/dev/bug/bankapp/rest/ApiController.java
+++ src/main/java/dev/bug/bankapp/rest/ApiController.java
@@ -1,5 +1,6 @@
 package dev.bug.bankapp.rest;
 
+import dev.bug.bankapp.repositories.AccountRepository;
 import dev.bug.bankapp.utils.ErrorMessageProvider;
 import dev.bug.bankapp.repositories.BankRepository;
 import dev.bug.bankapp.repositories.ClientRepository;
@@ -13,6 +14,9 @@ public abstract class ApiController {
 
     @Autowired
     protected ClientRepository clientRepository;
+
+    @Autowired
+    protected AccountRepository accountRepository;
 
     @Autowired
     protected ErrorMessageProvider messageProvider;
--- src/main/java/dev/bug/bankapp/rest/ClientController.java
+++ src/main/java/dev/bug/bankapp/rest/ClientController.java
@@ -1,38 +1,50 @@
 package dev.bug.bankapp.rest;
 
+import dev.bug.bankapp.dto.AccountDto;
 import dev.bug.bankapp.dto.ClientDto;
 import dev.bug.bankapp.exceptions.ClientNotFoundException;
+import dev.bug.bankapp.model.Account;
 import dev.bug.bankapp.model.Client;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
+@Api(tags = "Client Controller")
+@Transactional
 @RestController
-@RequestMapping(value = "clients",
-        consumes = "application/json",
-        produces = "application/json")
+@RequestMapping(value = "clients", consumes = "application/json")
 public class ClientController extends ApiController {
 
+    @ApiOperation(value = "Получить клиента по имени")
     @GetMapping("{name}")
     public ResponseEntity<ClientDto> getClientByName(@PathVariable String name) {
-        if(clientRepository.existsByName(name)) {
+        if (clientRepository.existsByName(name)) {
             ClientDto clientDto = mapper.clientTo(clientRepository.findByName(name));
             return ResponseEntity.ok(clientDto);
         }
         throw new ClientNotFoundException(messageProvider, name);
     }
 
+    @ApiOperation(value = "Найти клиентов содержащих в имени ...")
     @GetMapping("search")
-    public ResponseEntity<List<ClientDto>> searchClientsByName(@RequestParam String name) {
+    public ResponseEntity<List<ClientDto>> searchClientsByContainsName(@RequestParam String name) {
         List<Client> clients = clientRepository.findAllByNameContains(name);
-        if (clients != null || !clients.isEmpty()) {
-
+        if (!clients.isEmpty()) {
+            List<ClientDto> clientsDto = mapper.clientsTo(clients);
+            return ResponseEntity.ok(clientsDto);
         }
-        List<ClientDto> clientsDto = mapper.clientsTo(clients);
-        return clientsDto.isEmpty() ?
-                ResponseEntity.notFound().build() :
-                ResponseEntity.ok(clientsDto);
+        throw new ClientNotFoundException(messageProvider, name);
+    }
+
+    @ApiOperation(value = "Получить счета по имени клиента")
+    @GetMapping("{name}/accounts")
+    public ResponseEntity<List<AccountDto>> getAccountsByClientName(@PathVariable String name) {
+        List<Account> accounts = accountRepository.findAllByClientName(name);
+        List<AccountDto> accountsDto = mapper.accountsTo(accounts);
+        return ResponseEntity.ok(accountsDto);
     }
 }
