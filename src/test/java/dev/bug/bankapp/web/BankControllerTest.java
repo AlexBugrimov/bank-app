03ec31f68f0f1ed59fd411f9b7c8db3696960aac
Alex <bugrimov_a@bk.ru>
2020-02-10 00:16:54 +0300

    [#28] Написание тестов

--- src/test/java/dev/bug/bankapp/web/BankControllerTest.java
+++ src/test/java/dev/bug/bankapp/web/BankControllerTest.java
@@ -1,22 +1,31 @@
 package dev.bug.bankapp.web;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.bug.bankapp.dto.BankDto;
 import dev.bug.bankapp.model.Bank;
 import dev.bug.bankapp.services.BankService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyCollection;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BankController.class)
 class BankControllerTest {
@@ -26,6 +35,9 @@ class BankControllerTest {
 
     @MockBean
     private BankService bankService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void createdBankTest() throws Exception {
@@ -41,7 +53,7 @@ class BankControllerTest {
         mockMvc.perform(
                 post("/banks")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(bank)))
+                        .content(objectMapper.writeValueAsString(bank)))
                 .andDo(print())
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.bankId").exists())
@@ -50,5 +62,21 @@ class BankControllerTest {
 
     @Test
     void getClientNames() throws Exception {
+        long bankId = 100;
+        when(bankService.getClientNames(bankId)).thenReturn(Collections.singletonList("Иванов Иван"));
+
+        MvcResult result = mockMvc.perform(
+                get("/banks/" + bankId + "/client_names")
+        ).andExpect(status().is2xxSuccessful())
+                .andReturn();
+        List<String> names = objectMapper.readValue(
+                result.getResponse().getContentAsByteArray(),
+                new TypeReference<List<String>>() {
+                });
+        assertThat(names)
+                .contains("Иванов Иван");
+        assertThat(names.size())
+                .isEqualTo(1);
     }
+
 }
